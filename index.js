aaeb26993b29e527f9543dbd96526704dabbaa6c
Bilal-1337 <114568656+Bilal-1337@users.noreply.github.com>
2022-10-03 11:51:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('#btn')
+const img = document.getElementById('img')
+btn.addEventListener('click', () => {
+    img.classList.add('show')
+    
+})
+
+console.log ('yes');
